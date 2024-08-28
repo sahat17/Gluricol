f88710eecda5ae76626dc42207559e0c1174dc21
sahat17 <144116059+sahat17@users.noreply.github.com>
2024-08-28 18:01:56 +0700

    Update Analog Read Sensor.ino
    kode program untuk menampilkan suara

--- Analog Read Sensor.ino
+++ Analog Read Sensor.ino
@@ -1,27 +1,46 @@
 #include <WiFi.h>
+#include "DFRobotDFPlayerMini.h"
+#include "Arduino.h"
 
+#define FPSerial Serial2 //penggunaan UART2 alias rx pin 16 dan tx pin 17
 int sensor = 34;  // Pin sensor pada ESP32
 int glukosa;  // Variabel untuk kadar glukosa darah
 int uricAcid;  // Variabel untuk kadar asam urat
 int cholesterol;  // Variabel untuk kadar kolesterol
 
+DFRobotDFPlayerMini myDFPlayer;
+
 void setup() {
-  Serial.begin(9600);  // Inisialisasi komunikasi serial dengan baud rate 9600
+  FPSerial.begin(9600);  //memulai Serial2
+  Serial.begin(115200);  // Inisialisasi komunikasi serial dengan baud rate 9600
   pinMode(sensor, INPUT);  // Mengatur pin sensor sebagai input
+  myDFPlayer.volume(30);
 }
 
 void loop() {
+  
   int a = analogRead(sensor);  // Membaca nilai ADC dari sensor
   float voltage = a * (3.3 / 4095);  // Menghitung tegangan dari nilai ADC
 
   // Menghitung kadar glukosa darah dalam mg/dl
-  glukosa = (((-64.641 * voltage) + 188.17) - 22);
+  glukosa = (((-64.641 * voltage) + 300.17) - 22);
 
   // Menghitung kadar asam urat dalam mg/dl (disamakan dengan pendekatan gula darah)
-  uricAcid = (((-64.641 * voltage) + 188.17) - 22);
+  uricAcid = (((-64.641 * voltage) + 300.17) - 22);
 
   // Menghitung kadar kolesterol dalam mg/dl (disamakan dengan pendekatan gula darah)
-  cholesterol = (((-64.641 * voltage) + 188.17) - 22);
+  cholesterol = (((-64.641 * voltage) + 300.17) - 22);
+
+  if (gulaDarah < 100 && cholesterol < 200) {
+    myDFPlayer.play(1);  // Mainkan file 001_normal.mp3
+  } else if (gulaDarah >= 100) {
+    myDFPlayer.play(2);  // Mainkan file 002_guladarah.mp3
+  } else if (cholesterol>= 200) {
+    myDFPlayer.play(3);  // Mainkan file 003_kolesterol.mp3
+  }
+
+  delay(5000);  // Tunggu beberapa saat sebelum pengukuran berikutnya
+}
 
   if (glukosa < 0) {
     glukosa = 0;  // Jika hasil negatif, set ke 0
