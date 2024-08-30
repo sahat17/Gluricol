9dab8b94f50fc957f1889fc45390e7fda006ab7a
sahat17 <144116059+sahat17@users.noreply.github.com>
2024-08-30 14:24:27 +0700

    Mempersingkat kode program
    Perhitungan Kalibrasi sensor berdasarkan ADCnya, Memepersingkat kode program supaya storagenya tambah kecil

--- Kalibrasi ADC.ino
+++ Kalibrasi ADC.ino
@@ -1,17 +1,16 @@
-#include <WiFi.h>
-
 int sensor = 34;  // Pin sensor pada ESP32
-int glukosa;  // Variabel untuk kadar glukosa darah
-int uricAcid;  // Variabel untuk kadar asam urat
+
+int glukosa;      // Variabel untuk kadar glukosa darah
+int uricAcid;     // Variabel untuk kadar asam urat
 int cholesterol;  // Variabel untuk kadar kolesterol
 
 void setup() {
-  Serial.begin(9600);  // Inisialisasi komunikasi serial dengan baud rate 9600
+  Serial.begin(9600);      // Inisialisasi komunikasi serial dengan baud rate 9600
   pinMode(sensor, INPUT);  // Mengatur pin sensor sebagai input
 }
 
 void loop() {
-  int a = analogRead(sensor);  // Membaca nilai ADC dari sensor
+  int a = analogRead(sensor);        // Membaca nilai ADC dari sensor
   float voltage = a * (3.3 / 4095);  // Menghitung tegangan dari nilai ADC
 
   // Menghitung kadar glukosa darah dalam mg/dl
@@ -23,34 +22,21 @@ void loop() {
   // Menghitung kadar kolesterol dalam mg/dl (disamakan dengan pendekatan gula darah)
   cholesterol = (((-64.641 * voltage) + 188.17) - 22);
 
-  if (glukosa < 0) {
+  if (glukosa && uricAcid && cholesterol < 0) {
     glukosa = 0;  // Jika hasil negatif, set ke 0
+    uricAcid = 0;
+    cholesterol = 0;
   }
-
-  if (uricAcid < 0) {
-    uricAcid = 0;  // Jika hasil negatif, set ke 0
-  }
-
-  if (cholesterol < 0) {
-    cholesterol = 0;  // Jika hasil negatif, set ke 0
-  }
-
   // Menampilkan hasil di Serial Monitor
-  Serial.print("Kadar glukosa: ");
-  Serial.print(glukosa);
-  Serial.println(" mg/dl");
+String labels[] = {"Kadar glukosa: ", "Kadar asam urat: ", "Kadar kolesterol: ", "Tegangan sensor: "};
+  float values[] = {glukosa, uricAcid, cholesterol, voltage};
+  String units[] = {" mg/dl", " mg/dl", " mg/dl", " V"};
 
-  Serial.print("Kadar asam urat: ");
-  Serial.print(uricAcid);
-  Serial.println(" mg/dl");
-
-  Serial.print("Kadar kolesterol: ");
-  Serial.print(cholesterol);
-  Serial.println(" mg/dl");
-
-  Serial.print("Tegangan sensor: ");
-  Serial.print(voltage);
-  Serial.println(" V");
+  for (int i = 0; i < 4; i++) {
+    Serial.print(labels[i]);
+    Serial.print(values[i]);
+    Serial.println(units[i]);
+  }
 
   delay(1000);  // Menunggu 1 detik sebelum pembacaan berikutnya
 }
